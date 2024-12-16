d2bea800aa02b56ce2e04caabfaad5f278b4aee1
Mike Harrison <mike.harrison@selfridges.co.uk>
2024-12-16 11:36:45 +0000

    day 6 solution

--- 2024/day-6/index.ts
+++ 2024/day-6/index.ts
@@ -2,7 +2,7 @@ import { readFile } from "../readFile";
 
 const puzzleInput = readFile('./input.txt')
 
-let mapInput = puzzleInput.split('\n');
+const mapInput = puzzleInput.split('\n');
 
 function getGuardStartPosition(): number[] {
   for (let row = 0; row < mapInput.length; row++) {
@@ -15,11 +15,11 @@ function getGuardStartPosition(): number[] {
 }
 
 function moveGuardPosition(guardPosition: number[], positionsCovered: Map<string, number[]>) {
+  const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
   let directionIndex = 0;
-  const testDirections = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
   while (true) {
-    const nextPosition= [guardPosition[0] + testDirections[directionIndex][0], guardPosition[1] + testDirections[directionIndex][1]]
+    const nextPosition= [guardPosition[0] + directions[directionIndex][0], guardPosition[1] + directions[directionIndex][1]]
 
     const hasGuardLeftTheArea = mapInput[nextPosition[0]][nextPosition[1]] === undefined;
     if (hasGuardLeftTheArea) {
@@ -27,7 +27,7 @@ function moveGuardPosition(guardPosition: number[], positionsCovered: Map<string
     }
 
     if (mapInput[nextPosition[0]][nextPosition[1]] === '#') {
-      directionIndex = directionIndex === testDirections.length - 1 ? 0 : directionIndex + 1;
+      directionIndex = directionIndex === directions.length - 1 ? 0 : directionIndex + 1;
       continue;
     }
 
@@ -43,9 +43,70 @@ function getPatrolPathUniquePositions() {
 
   moveGuardPosition(guardPosition, positionsCovered);
 
-  return positionsCovered.size;
+  return positionsCovered;
 }
 
-console.log(getPatrolPathUniquePositions());
+// PART 2
+
+function isGuardStuckInLoop(map: Array<string>, guardPosition: number[], obstaclePosition: number[]) {
+  const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+  let directionIndex = 0;
+  const cache = new Map<string, boolean>();
+
+  while (true) {
+    cache.set(`${guardPosition[0]},${guardPosition[1]},${directionIndex}`, true);
+
+    const nextPosition= [guardPosition[0] + directions[directionIndex][0], guardPosition[1] + directions[directionIndex][1]]
+
+    const hasGuardLeftTheArea = nextPosition[0] >= map.length || nextPosition[0] < 0 || nextPosition[1] >= map[0].length || nextPosition[1] < 0;
+    if (hasGuardLeftTheArea) {
+      return false;
+    }
+
+    if (map[nextPosition[0]][nextPosition[1]] === '#') {
+      directionIndex = directionIndex === directions.length - 1 ? 0 : directionIndex + 1;
+      continue;
+    }
+
+    if (cache.has(`${nextPosition[0]},${nextPosition[1]},${directionIndex}`)) {
+      return true;
+    }
+
+    guardPosition = nextPosition;
+  }
+}
+
+function setObstacle(position: number[]) {
+  const mapWithObstacle = [...mapInput];
+  const row = [...mapInput[position[0]].split('')];
+
+  row[position[1]] = row[position[1]] !== '^' ? '#' : '^';
+  mapWithObstacle[position[0]] = row.join('');
+
+  return mapWithObstacle;
+}
+
+function getPatrolPathObstaclePositions() {
+  let guardPosition = getGuardStartPosition();
+  let count = 0;
+
+  for (let row = 0; row < mapInput.length; row++) {
+    for (let col = 0; col < mapInput[row].length; col++) {
+      const obstaclePosition = [row, col]
+
+      const mapInputWithCustomObstacle = setObstacle(obstaclePosition);
+
+      const isGuardStuck = isGuardStuckInLoop(mapInputWithCustomObstacle, guardPosition, obstaclePosition)
+      if (isGuardStuck) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+console.log(getPatrolPathUniquePositions().size);
+console.log(getPatrolPathObstaclePositions());
 
 
